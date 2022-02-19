79b0e55769713e6671bce0849c380e9bbb10aa26
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-19 14:40:18 +0000

    ready to start

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FunctionComponent = () => (
+  <div className="App">
+    <p>Hello world</p>
+  </div>
+);
 
 export default App;
