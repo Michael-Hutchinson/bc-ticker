1a41a19d751166cb7cb144ad3aa192c35aa7066e
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-19 15:55:03 +0000

    create data interface props
    - fetch data from api and log to console

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
+import { Wrapper } from './styles/App.styles';
 
-const App: React.FunctionComponent = () => (
-  <div className="App">
-    <p>Hello world</p>
-  </div>
-);
+interface IBitcoinDataProps {
+  '15m': number;
+  buy: number;
+  last: number;
+  sell: number;
+  symbol: string;
+}
+
+interface Currencies {
+  [key: string]: IBitcoinDataProps;
+}
+
+const getBCData = async (): Promise<Currencies> =>
+  (await fetch('https://blockchain.info/ticker')).json();
+
+const intervalTime = 30000;
+
+const App: React.FunctionComponent = () => {
+  const { data, isLoading, error, refetch } = useQuery<Currencies>(
+    'bc-data',
+    getBCData
+  );
+  console.log(data);
+  return (
+    <div className="App">
+      <p>Hello world</p>
+    </div>
+  );
+};
 
 export default App;
